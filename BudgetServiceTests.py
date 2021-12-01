759b323b6f1dd9bcc4a831d293aaecd23e5a070f
vans_li <vans_li@trendmicro.com>
2021-12-01 17:23:32 +0800

    test: final draft

--- BudgetServiceTests.py
+++ BudgetServiceTests.py
@@ -6,32 +6,57 @@ from buget_service import Budget, BudgetService
 
 class MyTestCase(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.s = BudgetService()
+
+    def fake_budgets(self):
+        return self.fake_data
+
     def test_no_data(self):
         start = date(2022, 12, 31)
         end = date(2022, 12, 31)
-        s = BudgetService()
         self.fake_data = []
-        s.get_all_budgets = self.fake_budgets
-        amount = s.query(start, end)
+        self.s.get_all_budgets = self.fake_budgets
+        amount = self.s.query(start, end)
         assert amount == 0
 
     def test_invalid_start_day(self):
         start = date(2022, 1, 1)
         end = date(2021, 12, 31)
         expected_amount = 0
-        s = BudgetService()
-        amount = s.query(start, end)
+        amount = self.s.query(start, end)
         assert amount == expected_amount
 
     def test_no_budget(self):
         self.fake_data = [Budget("202112", 0)]
-        budget_service = BudgetService()
-        budget_service.get_all_budgets = self.fake_budgets
-        result = budget_service.query(date(2021, 12, 31), date(2021, 12, 31))
+        self.s.get_all_budgets = self.fake_budgets
+        result = self.s.query(date(2021, 12, 31), date(2021, 12, 31))
         self.assertEqual(0, result)  # add assertion here
 
-    def fake_budgets(self):
-        return self.fake_data
+    def test_in_same_month(self):
+        self.fake_data = [Budget("202112", 31)]
+        self.s.get_all_budgets = self.fake_budgets
+        result = self.s.query(date(2021, 12, 1), date(2021, 12, 3))
+        self.assertEqual(3, result)
+
+    def test_cross_2_month(self):
+        self.fake_data = [Budget("202112", 31), Budget("202201", 62)]
+        self.s.get_all_budgets = self.fake_budgets
+        result = self.s.query(date(2021, 12, 31), date(2022, 1, 3))
+        self.assertEqual(7, result)
+
+    def test_cross_month(self):
+        self.fake_data = [Budget("202112", 31), Budget("202201", 62), Budget("202202", 0), Budget("202203", 93)]
+        self.s.get_all_budgets = self.fake_budgets
+        result = self.s.query(date(2021, 12, 31), date(2022, 3, 1))
+        self.assertEqual(66, result)
+
+    def test_cross_month_with_no_data(self):
+        self.fake_data = [Budget("202112", 31), Budget("202201", 62), Budget("202202", 0), Budget("202203", 93), Budget("202205", 31)]
+        self.s.get_all_budgets = self.fake_budgets
+        result = self.s.query(date(2022, 3, 1), date(2022, 5, 1))
+        self.assertEqual(94, result)
+
 
 
 if __name__ == '__main__':
--- buget_service.py
+++ buget_service.py
@@ -1,6 +1,8 @@
 import unittest
+from datetime import date
 
 from typing import List
+from calendar import monthrange, calendar
 
 
 class Budget:
@@ -17,15 +19,42 @@ class BudgetService:
     def get_all_budgets(self) -> List[Budget]:
         pass
 
+    def ratio(self, start, end, amount):
+        delta = (end - start).days + 1
+        num_days = monthrange(start.year, start.month)[1]
+        return delta * (amount // num_days)
+
     def query(self, start, end):
         budgets = self.get_all_budgets()
+        if start > end:
+            return 0
+
         if not budgets:
             return 0
 
-        if start > end:
-            return 0
+        if (start.year == end.year) and (start.month == end.month):
+            return self.ratio(start, end, budgets[0].amount)
+
+        if self.get_months(start, end)==1:
+            front_month = date(start.year, start.month, monthrange(start.year, start.month)[1])
+            last_month = date(end.year, end.month, 1)
+            return self.ratio(start, front_month, budgets[0].amount) + self.ratio(last_month, end, budgets[1].amount)
+
+        if self.get_months(start, end)>1:
+            front_month = date(start.year, start.month, monthrange(start.year, start.month)[1])
+            last_month = date(end.year, end.month, 1)
+
+            total = self.ratio(start, front_month, budgets[0].amount) + self.ratio(last_month, end, budgets[-1].amount)
+            for b in budgets[1:-1]:
+                total = total + b.amount
+            return total
+
         return budgets[0].amount
 
+    def get_months(self, start, end):
+        # month_delta = end.month - start.month + 12 * (end.year - start.year)
+        # month_list = pd.date_range(start.year, freq='M', period=month_delta)
+        return end.month - start.month + 12 * (end.year - start.year)
 
 if __name__ == '__main__':
     unittest.main()
